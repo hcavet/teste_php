d62db48cba3ffd71d1b6861db0afa79bacf47c71
hcavet <hcavet@outlook.com.br>
2019-04-30 11:39:48 -0300

    Add files via upload

--- processa.php
+++ processa.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 include_once("conexao.php");
 
 $SKU = filter_input(INPUT_POST, 'sku' FILTER_SANITIZE_STRING);
@@ -10,6 +10,13 @@ $preco = filter_input(INPUT_POST, 'preco', FILTER_SANITIZE_STRING);
 $result_produto = "INSERT INTO produtos (sku, nome, descricao, preco, created) VALUES ('$SKU', '$nome', '$descricao', '$preco' NOW())";
 
 $result_produto = mysql_query($conn, $result_produto);
+
+if (mysqli_insert_id ($conn)) {
+	$_SESSION ['msg'] = "Produto cadastrado com sucesso";
+	header("Location: index.php");
+} else {
+	header("Location: index.php");
+}
 
 
 ?>
